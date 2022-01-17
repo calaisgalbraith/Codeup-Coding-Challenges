afff63ce268aa80f7a996ffc2d69ea558e68b14c
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-17 11:33:04 -0600

    finished convertString challenge

--- src/amateur/ConvertString.java
+++
@@ -1,9 +0,0 @@
-package amateur;
-
-//Write a function to convert a string into an array of words.
-
-//Input: "Trick or Treat"
-//Output: [‘Trick’, ‘or’, ‘Treat’]
-
-public class ConvertString {
-}

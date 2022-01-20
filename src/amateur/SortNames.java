b4d72dbeac09d4fc12fcb2e047994a639c3b53c5
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-19 20:28:03 -0600

    added example to sortNames

--- src/amateur/SortNames.java
+++ src/amateur/SortNames.java
@@ -5,7 +5,7 @@ package amateur;
 // If the last names are the same sort them by the first name. Put the last name in front of the first name,
 // remove the colons and semicolons, put the names in parentheses and separate the names with commas.
 
-//Example:
+//Example: Travis:Meyer;Gene:Carangal;Tom:Young;Jeff:Meyer
 //The end string should look like this: "(CARANGAL, GENE)(MEYER, JEFF)(MEYER, TRAVIS)(YOUNG, TOM)"
 
 public class SortNames {
